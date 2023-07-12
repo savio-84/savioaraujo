aebb8c681d95bf9e517fa86396020398f98d7402
Sávio <saviocarvalho01@hotmail.com>
2023-07-12 15:29:37 -0300

    Link para o LinkedIn atualizado

--- src/pages/components/footer/index.tsx
+++ src/pages/components/footer/index.tsx
@@ -5,7 +5,7 @@ export function Footer() {
   return (
     <FooterComponent>
       <SocialMediaMenu>
-        <a href="https://www.linkedin.com/in/saviocarvalho84" target='_blank'>LinkedIn <LinkedinLogo size={20}/></a>
+        <a href="https://www.linkedin.com/in/saviocarvalho084/" target='_blank'>LinkedIn <LinkedinLogo size={20}/></a>
         <a href="https://www.instagram.com/saviocarvalho.ts/" target='_blank'>Instagram <InstagramLogo size={20}/></a>
       </SocialMediaMenu>
 
